2c03b1e3cd0b2ae9f93dae528bd48dc195fdda8b
leesj <seungju9788@gmail.com>
2025-06-30 10:48:14 +0900

    adidas dashboard

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+st.set_page_config(page_title="Adidas US Sales Dashboard", layout="wide")
+st.title("👟 Adidas US Sales Data Dashboard")
